cd9be209a0b5d323d101ba7f9252421d719aa20f
Luis02022 <160199124+Luis02022@users.noreply.github.com>
2025-05-27 11:19:35 -0300

    Delete src directory

--- src/main/java/com/example/aula/exception/JogadorJaCadastradoException.java
+++
@@ -1,7 +0,0 @@
-package com.example.aula.exception;
-
-public class JogadorJaCadastradoException extends RuntimeException {
-    public JogadorJaCadastradoException(String message) {
-        super(message);
-    }
-}
